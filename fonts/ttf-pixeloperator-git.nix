c9a687c850a867f38fe630421cb90218709ed582
jmarin217 <jmarin217@github.com>
2019-01-01 18:50:14 -0500

    added pixeloperator

---
+++ fonts/ttf-pixeloperator-git.nix
@@ -0,0 +1,18 @@
+{ stdenv, git }
+
+:stdenv.mkDerivation rec {
+    name = "ttf-pixeloperator";
+    version = "git";
+
+    src = builtins.fetchGit {
+      url = "https://notabug.org/HarvettFox96/ttf-pixeloperator.git";
+      rev = "496045526d00b66e37ca11762c446b7423145822";
+    };
+    
+    dontBuild = true;
+
+    installPhase = ''
+      mkdir -p $out/share/fonts/TTF
+      cp */*.ttf $out/share/fonts/TTF
+    '';
+}
